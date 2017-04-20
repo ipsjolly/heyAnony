dc531f4e0da7fbc168509ed0375ac677c1de7c95
ipsjolly <ipsj20@gmail.com>
2017-04-20 16:27:43 +0530

    ofc

--- heyAnonyLatest/gulpfile.js
+++ heyAnonyLatest/gulpfile.js
@@ -57,6 +57,24 @@ gulp.task('template', function(done){
     .pipe(templateCache({
         standalone:true,
         root: 'templates'}))
-    .pipe(gulp.dest('./public'))
+    .pipe(gulp.dest('./www/dist'))
     .on('end', done);
+});
+
+
+var ngAnnotate = require('gulp-ng-annotate');
+var useref = require('gulp-useref');
+var uglify = require("gulp-uglify");
+gulp.task('html', function (done) {
+  return gulp.src('./www/*.html')
+        .pipe(useref())
+        .pipe(gulp.dest('./www/dist'));
+});
+
+gulp.task('uglify', function() {
+  gulp.src('./www/js/*.js')
+    .pipe(uglify('ugly.js', {
+      outSourceMap: true
+    }))
+    .pipe(gulp.dest('./www/dist'))
 });
